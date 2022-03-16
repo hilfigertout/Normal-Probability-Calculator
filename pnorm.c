d684b436d714b98f4c408afdd443436cb2f65cbc
Ian Roberts <ian@Leviticus.home>
2022-03-16 14:23:48 -0400

    Got calculator working for values close to mean

--- pnorm.c
+++ pnorm.c
@@ -1,13 +1,15 @@
 //This program calculates the probability from a normal distribution
-//with mean "mu" and standard deviation "sigma". 
+//with mean "mu" and standard deviation "sigma".
 //If mu and sigma are not specified, they will be assumed to be 0 and 1 respectively.
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
 
-#define SERIES_TERMS 10
+#define MIN_SERIES_TERMS 15
 #define INVERSE_SQRT_2PI 0.3989422804
+#define HIGHEST_Z 8
+#define LOWEST_Z -8
 
 double zScore(double x, double mu, double sigma) {
 	return ((x-mu)/sigma);
@@ -15,18 +17,39 @@ double zScore(double x, double mu, double sigma) {
 
 
 double normCDF(double z) {
-	int i = 0;
-	double current_num = z;
-	int factorial_term = 1;
-	
-}
-
-double normProb(double z_low, double z_high) {
-	
+	double output = z; //starting point, computed term at n=0
+	int n = 0;
+	double current_numerator = z;
+	unsigned long denominator = 1;
+	long denominator_piece = 1;
+	if (z > HIGHEST_Z) {
+		output = 1;
+	} else if (z < LOWEST_Z) {
+		output = 0;
+	} else if (z == 0.0) {
+		output = 0.5;
+	} else {
+		int total_terms = MIN_SERIES_TERMS;
+		if (z*5 > MIN_SERIES_TERMS) {
+			total_terms = z*5;
+		}
+		//For loop used to compute SUM((Z^(2n+1)(-1)^n)/((2n+1)(2^n)(n!))) in linear time
+		for(n=1; n <= total_terms; n++) {
+			current_numerator = current_numerator*z*z*(-1);
+			denominator_piece = denominator_piece*n; //factorial term
+			denominator_piece = denominator_piece<<1; //2^n term
+			denominator = (2*n+1)*denominator_piece; //(2n+1) term
+			output = output + (current_numerator / (double)denominator);
+		}
+		output *= INVERSE_SQRT_2PI;
+		output += 0.5;
+	}
+	return output;
 }
 
 
 int main(int argc, char** argv) {
+	//Read user input
 	char* endptr = NULL; //This is for error checking
 	if (argc < 3) {
 		printf("ERROR: not enough arguments.\n");
@@ -72,17 +95,16 @@ int main(int argc, char** argv) {
 		exit(1);
 	}
 
-//	printf("low_value = %f\n", lowX);
-//	printf("high_value = %f\n", highX);
-//	printf("mu = %f\n", mu);
-//	printf("sigma = %f\n", sigma);
-
-	
+	//actual calculations start here
+	double output = 0;
 	if (lowX == highX) {
-		return 0.0;
+		output = 0.0;
 	} else {
-		lowZ = zScore(lowX, mu, sigma);
-		highZ = zScore(highX, mu, sigma);
-		return normProb(lowZ, highZ);
+		double lowZ = zScore(lowX, mu, sigma);
+		double highZ = zScore(highX, mu, sigma);
+		output = normCDF(highZ) - normCDF(lowZ);
 	}
+	//Output result
+	printf("P(%f < X < %f) = %f\n", lowX, highX, output);
+	return 0;
 }
